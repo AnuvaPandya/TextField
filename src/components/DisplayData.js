ad35f26023a1318d262bf3d7c35ef496959bcf10
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-11-10 17:58:59 +0530

    new change

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -3,7 +3,7 @@ import { TextField } from "@mui/material";
 
 const DisplayData = () => {
   const [data, setData] = useState([]);
-  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -14,22 +14,27 @@ const DisplayData = () => {
   return (
     <div>
       <TextField
+        style={{
+          padding: "20px",
+          margin: "2rem",
+        }}
         id="outlined-basic"
         label="Search"
         variant="outlined"
-        onChange={(event) => setId(event.target.value)}
+        onChange={(event) => setTitle(event.target.value)}
       />
       <div>
-        {data.map((item) => {
-          return (
-            <>
-              <div>
-                {item.id} ) {item.title.toUpperCase()}
-                {data.filter((item) => item.title.toUpperCase().includes())}
-              </div>
-            </>
-          );
-        })}
+        {data
+          .filter((item) => item.title.toUpperCase().includes())
+          .map((item) => {
+            return (
+              <>
+                <div>
+                  {item.id} ) {item.title.toUpperCase()}
+                </div>
+              </>
+            );
+          })}
       </div>
     </div>
   );
