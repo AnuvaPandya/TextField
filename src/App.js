e1c439967acadc4ec3eac9ff166e1b86f92edb0d
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-11-15 15:48:48 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css";
 import { useState, useEffect } from "react";
 import DisplayData from "./components/DisplayData";
 import Loading from "./components/Loading";
--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -7,20 +7,20 @@ const DisplayData = () => {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
 
-  const newPosts = async () => {
-    try {
-      const userPosts = await fetch(
-        "https://jsonplaceholder.typicode.com/comments"
-      );
-      setPosts(userPosts);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const newPosts = async () => {
+  //   try {
+  //     const userPosts = await fetch(
+  //       "https://jsonplaceholder.typicode.com/comments"
+  //     );
+  //     setPosts(userPosts);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    newPosts();
-  });
+  // useEffect(() => {
+  //   newPosts();
+  // });
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -30,19 +30,24 @@ const DisplayData = () => {
 
   return (
     <div>
-      <TextField
-        style={{
-          padding: "3px",
-          margin: "1rem",
-          display: "flex",
-        }}
-        id="outlined-basic"
-        label="Search"
-        variant="outlined"
-        onChange={(event) => setTitle(event.target.value)}
-      />
+      <div className="p-3 m-1 flex">
+        <TextField
+          // style={{
+          //   padding: "3px",
+          //   margin: "1rem",
+          //   display: "flex",
+          // }}
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+        Refresh with new API
+      </button>
       <div>
-        {posts.map((item) => {
+        {/* {posts.map((item) => {
           return (
             <>
               <div>
@@ -50,14 +55,14 @@ const DisplayData = () => {
               </div>
             </>
           );
-        })}
+        })} */}
 
         {data
           .filter((item) => item.title.toUpperCase().includes(title))
           .map((item) => {
             return (
               <>
-                <div>
+                <div classname="border-2 m-1 p-2">
                   {item.id} ) {item.title.toUpperCase()}
                 </div>
               </>
